21299b85f9c11f0663a8fa4d91ac297c02ac5cf3
Pavel <palllkaignatev@gmail.com>
2025-04-08 15:31:44 +0300

    Release

--- src/db/logs.ts
+++ src/db/logs.ts
@@ -10,7 +10,7 @@ import { getAccountById } from "./accounts";
 import { cleanString } from "../helpers/cleanString";
 
 const getLogsCollection = async () => {
-  return (await logsDB()).collection("fucker_logs");
+  return (await logsDB()).collection("logs");
 };
 
 export const getUniquePrefixes = async () => {
